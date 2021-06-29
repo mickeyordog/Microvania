05a6ad95862bc80fb9e5ac8e68cd940790edd438
Mickey Ordog <mickeyordog@gmail.com>
2021-06-29 00:32:23 -0700

    Add player stretch and squash upon jumping

--- Microvania/Assets/Scripts/PlayerMovement.cs
+++ Microvania/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float jumpSpeed = 10f;
-    public LayerMask whatIsGround;
     public float walkSpeed = 10f;
+
+    public Transform groundCheck;
+    public float groundedRadius;
+    public LayerMask whatIsGround;
 
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sr;
 
+    private bool isGrounded;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
@@ -28,9 +33,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            anim.SetTrigger("jumpTrigger");
         }
         float horizontalVelocity = Input.GetAxisRaw("Horizontal");
         if (horizontalVelocity < 0f)
@@ -41,5 +47,35 @@ public class PlayerMovement : MonoBehaviour
             sr.flipX = false;
         }
         rb.velocity = new Vector2(horizontalVelocity * walkSpeed, rb.velocity.y);
+
+        // this block should be in fixedUpdate
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+        anim.ResetTrigger("landedTrigger");
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                if (!wasGrounded)
+                {
+                    Debug.Log("Landed");
+                    anim.SetTrigger("landedTrigger");
+
+                }
+                break;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundedRadius);
     }
 }
